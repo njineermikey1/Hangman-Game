48d65bd8c08be5e3d6f0c30009ccd16125d4151c
njineermikey1 <95704652+njineermikey1@users.noreply.github.com>
2023-04-06 10:51:17 -0700

    Added game continuation
    Filling in the spaces with the correct letters was added

--- main.py
+++ main.py
@@ -10,37 +10,43 @@ word_list = ["aardvark", "baboon", "camel", "pistachio","mountain","radio","tumb
 import random
 #random.seed(1234)
 #TODO-1
+continue_game = "y"
 chosen_word = random.choice(word_list)
 chsen_word = chosen_word.lower()
 print(chosen_word)
-
-#TODO-2
-guess = input("Guess a letter: ")
-guess = guess.lower()
-print(guess)
-word_length = len(chosen_word)
-print(word_length)
 underscore_char = "_"
 underscore_word = underscore_char * len(chosen_word)
 print(chosen_word)
 print(underscore_word)
-#my_string = "Hello World"
-#index_to_replace = 4 = x
-#new_char = "a" = underscore_char
-#new_string = my_string[:index_to_replace] + new_char + my_string[index_to_replace+1:] - underscor_word = underscore_word[x] + guess + underscore_word[x+1]
-#print(new_string)
-y = 0
-for x in chosen_word:
-  if x == guess:
-    print("Match " + guess + " = " + x)
-    underscore_word = underscore_word[:y] + guess + underscore_word[y+1:]
-    print(underscore_word)
-    y += 1
-  else:
-    print("No Match " + guess + " != " + x)
-    y += 1
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
+#TODO-2
+while continue_game == "y":
+  guess = input("Guess a letter: ")
+  guess = guess.lower()
+  print(guess)
+  word_length = len(chosen_word)
+  print(word_length)
+  
+  #my_string = "Hello World"
+  #index_to_replace = 4 = x
+  #new_char = "a" = underscore_char
+  #new_string = my_string[:index_to_replace] + new_char + my_string[index_to_replace+1:] - underscor_word = underscore_word[x] + guess + underscore_word[x+1]
+  #print(new_string)
+
+  y = 0
+  missing = 0
+  for x in chosen_word:
+    if x == guess:
+      print("Match " + guess + " = " + x)
+      underscore_word = underscore_word[:y] + guess + underscore_word[y+1:]
+      print(underscore_word)
+      y += 1
     else:
-        print("Wrong")
+      print("No Match " + guess + " != " + x)
+      y += 1
+      missing += 1
+  #for letter in chosen_word:
+      #if letter == guess:
+          #print("Right")
+      #else:
+          #print("Wrong")
+continue_game = input("Continue?")
